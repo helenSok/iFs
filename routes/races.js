5bffdb3916510ddcc8c10850a910525e3e51fd92
helenSok <50926136+helenSok@users.noreply.github.com>
2021-05-28 17:38:46 +0200

    Delete races.js

--- routes/races.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const controller = require("../controllers/races")
-
-router.get('/races', controller.getAll)
-
-module.exports = router
