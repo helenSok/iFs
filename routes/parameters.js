827485fc1d4300d71649aa2b40b36c414ce28464
helenSok <50926136+helenSok@users.noreply.github.com>
2021-05-28 17:38:40 +0200

    Delete parameters.js

--- routes/parameters.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const controller = require("../controllers/parameters")
-
-router.get('/parameters', controller.getAll) //Пользовательские настройки упражнения
-router.put('/parameters/:id', controller.update)
-
-module.exports = router
