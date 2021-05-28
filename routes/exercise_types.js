d05cbfe5374013af5241407b7e601189cc35bb76
helenSok <50926136+helenSok@users.noreply.github.com>
2021-05-28 17:38:18 +0200

    Delete exercise_types.js

--- routes/exercise_types.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const controller = require("../controllers/exercise_types")
-
-router.get('/exercise_types', controller.getAll) //Пользовательские настройки упражнения
-
-module.exports = router
