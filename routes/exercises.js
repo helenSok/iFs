620b10601170703da0070a5ad852c7c61440c2a1
helenSok <50926136+helenSok@users.noreply.github.com>
2021-05-28 17:38:24 +0200

    Delete exercises.js

--- routes/exercises.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const controller = require("../controllers/exercises")
-
-router.get('/exercises/settings/users/:id', controller.getAll) //Пользовательские настройки упражнения
-
-module.exports = router
